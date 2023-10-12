b3e64da6fb9479aed671122cc4afc246fee9eb20
stupidcabbage <vanya@gutche.ru>
2023-10-12 17:50:41 +0300

    Исправление ошибок при работе с дневником

--- diary/handlers/diary.py
+++ diary/handlers/diary.py
@@ -3,10 +3,18 @@ from aiogram.filters import Command, CommandObject
 from aiogram.types import Message
 from api.diary import get_lessons
 from templates import render_template
-
 from dateparser import parse
 from services.user import test_user
 router = Router()
+
+
+def parse_date(date: str) -> str | None:
+    parsed_date = parse(date,
+                        settings={"TIMEZONE": "Asia/Yekaterinburg",
+                                  "PREFER_DATES_FROM": "future"})
+    if not parsed_date:
+        return None
+    return parsed_date.strftime("%d.%m.%Y")
 
 
 @router.message(Command("diary"))
@@ -14,13 +22,17 @@ async def get_tomorrow_lessons(message: Message,
                                command: CommandObject) -> None:
     if not command.args:
         diary = await get_lessons(test_user)
+        if not diary:
+            diary = await get_lessons(test_user, parse_date("понедельник"))
         await message.answer(await render_template("diary.j2", {"diary": diary}))
     else:
-        date = parse(command.args,
-                     settings={"TIMEZONE": "Asia/Yekaterinburg",
-                               "PREFER_DATES_FROM": "future"})
+        date = parse_date(command.args)
         if not date:
             await message.answer("Время указано неправильно!")
         else:
-            diary = await get_lessons(test_user, date.strftime("%d.%m.%Y"))
-            await message.answer(await render_template("diary.j2", {"diary": diary}))
+            diary = await get_lessons(test_user, date)
+            if not diary:
+                await message.answer(await render_template("nolessons.j2", {"date": date}))
+                await message.answer_sticker("https://i.imgur.com/gYYgAih.mp4")
+            else:
+                await message.answer(await render_template("diary.j2", {"diary": diary}))
--- diary/services/user.py
+++ diary/services/user.py
@@ -9,6 +9,7 @@ from diary.config import GOSUSLUGI_LOGIN, GOSUSLUGI_PASSWORD
 class User:
     username: LiteralString
     password: LiteralString
+    telegram_id: int
     parcipiant_id: str | None = None
     cookies: dict | None = None
 
@@ -16,5 +17,6 @@ class User:
 test_user = User(
         username=GOSUSLUGI_LOGIN,
         password=GOSUSLUGI_PASSWORD,
+        telegram_id=1122659029,
         parcipiant_id="A7A48C5F8B939B82826487956E3FA893",
         cookies={"PHPSESSID": "1cdefad789a784d074daae150d782b0f"})
